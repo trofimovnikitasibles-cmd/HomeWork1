7bd8dc2ebd35c9807095c4069654171baa34502a
Никита Трофимов <Trofimov.nikita.sibles@gmail.com>
2026-05-20 13:07:29 +0300

    HW#7 встроенные объекты (задание #9)

--- HW-2.js
+++ HW-2.js
@@ -649,3 +649,9 @@ console.log(getRandomInRange(5, 15));
 
 //#8
 console.log(new Date());
+
+//#9
+
+const currentDate = new Date();
+currentDate.setDate(currentDate.getDate() + 73);
+console.log(currentDate.toLocaleDateString());
