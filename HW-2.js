e36a804ddb24579d7541fac2f470512424f408cd
Никита Трофимов <Trofimov.nikita.sibles@gmail.com>
2026-05-28 15:01:39 +0300

    HW#8 Callback, setTimeout, setInterval (задание #5)

--- HW-2.js
+++ HW-2.js
@@ -757,4 +757,24 @@ delayForSecond(function () {
    console.log('Привет, Глеб!');
 });
 
+// #5
+
+// Функция delayForSecond через 1 секунду пишет в консоль 
+// «Прошла одна секунда», а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi (name) {
+    console.log('Привет, ${name}!');
+}
+
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+delayForSecond(() => sayHi('Глеб'));
 
