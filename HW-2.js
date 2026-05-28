ecb6f7a39a5a62284dee0ad90dca3cc45d0b70d0
Никита Трофимов <Trofimov.nikita.sibles@gmail.com>
2026-05-28 14:13:05 +0300

    HW#8 Callback, setTimeout, setInterval (задание #1)

--- HW-2.js
+++ HW-2.js
@@ -698,6 +698,15 @@ if (compChoice === userChoice){
 }
 
 // #1
+const people = [
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
+
+
+console.log(people.sort((a,b) => a.age - b.age));
 
 
 
